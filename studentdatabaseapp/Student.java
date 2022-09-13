843814c96017ff64d8ca9363af0a8225dfe29723
4rlm <4rlm@protonmail.ch>
2022-09-13 17:42:20 +0900

    create unique id and get first and last name

--- studentdatabaseapp/Student.java
+++ studentdatabaseapp/Student.java
@@ -1,17 +1,41 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
   private String firstName;
   private String lastName;
-  private String gradeYear;
+  private int gradeYear;
   private int studentID;
   private String courses;
   private int tuitionBalance;
-  private int costOfCourse = 600;
+  private static int costOfCourse = 600;
+  private static int id = 1000;
 
   // Constructor: prompt user to enter name and year
+  public Student() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter student first name: ");
+    this.firstName = in.nextLine();
+
+    System.out.print("Enter student last name: ");
+    this.lastName = in.nextLine();
+
+    System.out.print("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+    this.gradeYear = in.nextInt();
+
+    setStudentID();
+
+    System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+  }
 
   // Generate an ID
+  private void setStudentID() {
+    // Grade Level + ID
+    id++;
+    this.studentID = Integer.parseInt(Integer.toString(gradeYear) + Integer.toString(id));
+  }
 
   // Enroll in courses
 
--- studentdatabaseapp/StudentDatabaseApp.java
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,7 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
   public static void main(String[] args) {
+    Student stu1 = new Student();
     // Ask the user how many new students will be added to the database
 
     // Create a number of students to loop through
