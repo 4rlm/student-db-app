fa156335630df0c73c3ccedb821d4ea09ed58914
4rlm <4rlm@protonmail.ch>
2022-09-13 16:59:58 +0900

    add specs and variables

--- studentdatabaseapp/StudentDatabaseApp.java
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -3,6 +3,9 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
   public static void main(String[] args) {
+    // Ask the user how many new students will be added to the database
+
+    // Create a number of students to loop through
 
   }
 
