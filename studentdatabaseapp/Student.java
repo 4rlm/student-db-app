7fb537e152f029d8383a8949993bdb907b28e4c3
4rlm <4rlm@protonmail.ch>
2022-09-14 05:45:40 +0900

    create array of objects to enroll

--- studentdatabaseapp/Student.java
+++ studentdatabaseapp/Student.java
@@ -1,5 +1,6 @@
 package studentdatabaseapp;
 
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Student {
@@ -7,7 +8,7 @@ public class Student {
   private String lastName;
   private int gradeYear;
   private int studentID;
-  private String courses = null;
+  private String courses;
   private int tuitionBalance = 0;
   private static int costOfCourse = 600;
   private static int id = 1000;
@@ -25,9 +26,6 @@ public class Student {
     this.gradeYear = in.nextInt();
 
     setStudentID();
-
-    System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
-
   }
 
   // Generate an ID
@@ -47,14 +45,12 @@ public class Student {
       String course = in.nextLine();
 
       if (!course.equals("Q")) {
-        courses = course + "\n" + course;
+        courses = courses + "\n " + course;
         tuitionBalance = tuitionBalance + costOfCourse;
       } else {
         break;
       }
     } while (1 != 0);
-
-    System.out.println("ENROLLED IN: " + courses);
   }
 
   // View balance
@@ -74,5 +70,12 @@ public class Student {
   }
 
   // Show status
+  public String showInfo() {
+    return "Name: " + firstName + " " + lastName +
+        "\nGrade level: " + gradeYear +
+        "\nStudent ID: " + studentID +
+        "\nCourses Enrolled: " + courses +
+        "\nBalance: $" + tuitionBalance;
+  }
 
 }
--- studentdatabaseapp/StudentDatabaseApp.java
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -1,14 +1,28 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
   public static void main(String[] args) {
-    Student stu1 = new Student();
-    stu1.enroll();
-    stu1.payTuition();
-    // Ask the user how many new students will be added to the database
+
+    // Ask how many new students to enroll
+    System.out.print("Enter number of new students to enroll: ");
+    Scanner in = new Scanner(System.in);
+
+    int numOfStudents = in.nextInt();
+    Student[] students = new Student[numOfStudents];
 
     // Create a number of students to loop through
+    for (int n = 0; n < students.length; n++) {
+      students[n] = new Student();
+      students[n].enroll();
+      students[n].payTuition();
+    }
+
+    for (int i = 0; i < students.length; i++) {
+      System.out.println(students[1].showInfo());
+    }
 
   }
 }
